08ae31c4b256e77b17c450f5f7ed11995200ce05
William Bushie <willbushie@gmail.com>
2022-03-05 13:01:00 -0600

    intitial test commit

---
+++ test.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+public class test {
+
+    private static void createAndShowGUI() {
+        JFrame jFrame = new JFrame("Hello World Swing Example");
+        jFrame.setLayout(new FlowLayout());
+        jFrame.setSize(500, 360);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JLabel label = new JLabel("Hello World Swing");
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        label.setBorder(border);
+        label.setPreferredSize(new Dimension(150, 100));
+
+        label.setText("Hello World Swing");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+
+        jFrame.add(label);
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        createAndShowGUI();
+    }
+}
