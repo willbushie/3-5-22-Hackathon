52fe40ab14fe192e486d6fb1d621863b06a058f4
William Bushie <willbushie@gmail.com>
2022-03-05 13:18:29 -0600

    added some photots, and created 'chessgame.java' file.

---
+++ chessgame.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+public class chessgame {
+    
+    
+
+    // show the chess board
+    public static void main(String[] args) {
+        createAndShowGUI();
+    }
+
+}
